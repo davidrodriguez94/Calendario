7fdfb937868cc96c12be7e385a577d7db52295af
davidrodriguez94 <rodriguezdiezdavid94@gmail.com>
2017-10-31 09:54:16 +0100

    Constructor añadido

---
+++ calendario.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class calendario here.
+ *
+ * @author (David Rodriguez Diez)
+ * @version (a version number or a date)
+ */
+public class calendario
+{
+    // Iniciando variables dia, mes y año
+    private int dia;
+    private int mes;
+    private int anno;
+
+    /**
+     * Constructor for objects of class calendario
+     */
+    public calendario()
+    {
+        dia = 1;
+        mes = 1;
+        anno = 1;
+    }
+
+}
